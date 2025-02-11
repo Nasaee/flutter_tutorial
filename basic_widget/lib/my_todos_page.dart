cd0ae295cc28b0fc1319db0a543026d0844ae3d8
Nasaee <nasaee.dev@gmail.com>
2025-02-11 22:50:17 +0700

    todos app

--- basic_widget/lib/my_todos_page.dart
+++ basic_widget/lib/my_todos_page.dart
@@ -8,6 +8,9 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+  final _controller = TextEditingController();
+  TodoPriority priority = TodoPriority.normal;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +21,7 @@ class _TodoPageState extends State<TodoPage> {
         ),
         backgroundColor: Colors.blue[300],
         onPressed: () {
-          // Add your code here
+          addTodo();
         },
         child: Icon(
           Icons.add,
@@ -47,9 +50,115 @@ class _TodoPageState extends State<TodoPage> {
             ),
     );
   }
+
+  void addTodo() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setBuilderState) => Padding(
+          padding: const EdgeInsets.only(
+            top: 20.0,
+            left: 16.0,
+            right: 16.0,
+            bottom: 16.0,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'What to do?',
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Select Piority'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Radio<TodoPriority>(
+                    value: TodoPriority.low,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.low.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.normal,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.normal.name),
+                  Radio<TodoPriority>(
+                    value: TodoPriority.high,
+                    groupValue: priority,
+                    onChanged: (value) {
+                      setBuilderState(() {
+                        priority = value!;
+                      });
+                    },
+                  ),
+                  Text(TodoPriority.high.name),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: _save,
+                child: Text('Save'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _save() {
+    if (_controller.text.isEmpty) {
+      showMsg(context, 'Input field cannot be empty');
+      return;
+    }
+    final todo = MyTodo(
+      id: MyTodo.todos.length + 1,
+      name: _controller.text,
+      priority: priority,
+    );
+
+    MyTodo.todos.add(todo);
+    setState(() {
+      _controller.clear();
+      Navigator.pop(context);
+    });
+  }
 }
 
-enum TodoPriority { low, medium, high }
+void showMsg(BuildContext context, String s) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Caution!'),
+      content: Text(s),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('CLOSE'),
+        ),
+      ],
+    ),
+  );
+}
+
+enum TodoPriority { low, normal, high }
 
 class TodoItem extends StatelessWidget {
   final MyTodo todo;
