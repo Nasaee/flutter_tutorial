1f0e04fb71b7b9a60b12e50c084166aa7009150b
Nasaee <nasaee.dev@gmail.com>
2025-03-07 13:23:26 +0700

    setup home screen

--- earthquake/lib/providers/app_data_provider.dart
+++ earthquake/lib/providers/app_data_provider.dart
@@ -1,6 +1,7 @@
 import 'package:earthquake/models/earthquake_model.dart';
 import 'package:earthquake/utils/helper_functions.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class AppDataProvider extends ChangeNotifier {
   final baseUrl = Uri.parse('https://earthquake.usgs.gov/fdsnws/event/1/query');
@@ -46,5 +47,35 @@ class AppDataProvider extends ChangeNotifier {
     _endTime = getFormattedDateTime(DateTime.now().millisecondsSinceEpoch);
     _maxRadiusKm = maxRadiusKmThreshold;
     _setQueryParams();
+    getEarthquakeData();
+  }
+
+  Future<void> getEarthquakeData() async {
+    /*
+    authority: earthquake.usgs.gov (not include https://)
+    path: /fdsnws/event/1/query
+    queryParameters: {
+      format: geojson,
+      starttime: 2021-10-01,
+      endtime: 2021-10-02,
+      minmagnitude: 4,
+      orderby: time,
+      limit: 500,
+      latitude: 0.0,
+      longitude: 0.0,
+      maxradiuskm: 500
+    }
+    */
+    final uri = Uri.https(baseUrl.authority, baseUrl.path, queryParams);
+    try {
+      final response = await http.get(uri);
+      if (response.statusCode == 200) {
+        earthquakeModel = EarthquakeModel.fromJson(response.body);
+        print(earthquakeModel?.features?.length);
+        notifyListeners();
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
   }
 }
